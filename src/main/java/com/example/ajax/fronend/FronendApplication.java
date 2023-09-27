f6683c1fe98e3465dccf775e1491ccf1a90d4d29
zawlinshein <pudgeismyfavourate@gmail.com>
2023-09-27 10:48:21 +0630

    initalize git -> ajax testing

---
+++ src/main/java/com/example/ajax/fronend/FronendApplication.java
@@ -0,0 +1,13 @@
+package com.example.ajax.fronend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FronendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FronendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ajax/fronend/FronendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ajax.fronend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FronendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
